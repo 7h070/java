bfa7cb699dc724d85f7ebf53c1066f83800ce5e9
OTO <l7h070l@gmail.com>
2011-02-06 00:46:12 +0000

    test3

--- src/test.java
+++ src/test.java
@@ -3,6 +3,6 @@ public class test {
 	public static void main(String[] ar){
 		System.out.println("Test Git!");
 		System.out.println("Test Git!");
-		System.out.println("Test Git!");
+		System.out.println("Test Git!");///
 	}
 }
